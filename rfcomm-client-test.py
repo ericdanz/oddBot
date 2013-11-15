e596af93dba1c442eed7651832d712e1cc613447
Eric Danz <ericdanziger@gmail.com>
2013-11-15 05:23:54 -0500

    formatting

--- rfcomm-client-test.py
+++ rfcomm-client-test.py
@@ -6,19 +6,11 @@ service_matches = bluetooth.find_service(uuid = muuid)
 
 if len(service_matches) == 0:
 	print "No matches for insecure"
+	sys.exit(0)
 else:
 	for match in service_matches:
 		print match
-		print "{'name'} {1} {2}".format(*match)
+		print "{name} {host} {port}".format(**match)
 
 		
-muuid = "fa87c0d0-afac-11de-8a39-0800200c9a66"
-service_matches = bluetooth.find_service(uuid = muuid)
-
-if len(service_matches) == 0:
-	print "No matches for secure"
-	sys.exit(0)
-	
-for match in service_matches:
-	print match
-	print "{0} {1} {2}".format(*match)
+sock = bluetooth.
