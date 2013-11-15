0ab7bd19cf749cc041533c797716c6227d77a476
Eric Danz <ericdanziger@gmail.com>
2013-11-15 05:08:03 -0500

    changed uuids

--- rfcomm-client-test.py
+++ rfcomm-client-test.py
@@ -1,7 +1,7 @@
 import sys
 import bluetooth
 
-muuid = "insecure"
+muuid = "8ce255c0-200a-11e0-ac64-0800200c9a66"
 service_matches = bluetooth.find_service(uuid = muuid)
 
 in len(service_matches) == 0:
@@ -11,7 +11,7 @@ in len(service_matches) == 0:
 for match in service_matches:
 	print "{name} {port} {host}".format(*match)
 
-muuid = "secure"
+muuid = "fa87c0d0-afac-11de-8a39-0800200c9a66"
 service_matches = bluetooth.find_service(uuid = muuid)
 
 in len(service_matches) == 0:
