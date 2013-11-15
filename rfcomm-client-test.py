256c5de437da4f51dcf920f7b388bf00feeac5c7
Eric Danz <ericdanziger@gmail.com>
2013-11-15 04:13:48 -0500

    rf-comm client search sdp

--- rfcomm-client-test.py
+++ rfcomm-client-test.py
@@ -5,10 +5,18 @@ muuid = "insecure"
 service_matches = bluetooth.find_service(uuid = muuid)
 
 in len(service_matches) == 0:
-	print "No matches"
+	print "No matches for insecure"
 	sys.exit(0)
 	
 for match in service_matches:
 	print "{name} {port} {host}".format(*match)
 
+muuid = "secure"
+service_matches = bluetooth.find_service(uuid = muuid)
+
+in len(service_matches) == 0:
+	print "No matches for secure"
+	sys.exit(0)
 	
+for match in service_matches:
+	print "{name} {port} {host}".format(*match)
