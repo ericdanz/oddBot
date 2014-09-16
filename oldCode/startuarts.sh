d761b8da6ed3707514425afb66c1528c6b31ade8
Eric D <ericdanziger@cmu.edu>
2014-09-16 00:06:57 -0400

    consolidating to new format

--- oldCode/startuarts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo enable-uart2 > /sys/devices/bone_capemgr.9/slots
-echo enable-uart3 > /sys/devices/bone_capemgr.9/slots
-echo enable-uart5 > /sys/devices/bone_capemgr.9/slots
--- oldCode/startuartsang.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo BB-UART4 > /sys/devices/bone_capemgr.9/slots
-echo BB-UART2 > /sys/devices/bone_capemgr.9/slots
-echo BB-UART1 > /sys/devices/bone_capemgr.9/slots
-
